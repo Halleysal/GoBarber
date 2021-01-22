21cbe678c138c5d1f8198b72655455045da9d4fa
Halley SG <halleysal@gmail.com>
2021-01-21 22:06:23 -0300

    chore: adding month and day availability routes

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express';
 
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticaded';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticaded);
 
 providersRouter.get('/', providersController.create);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.create,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.create,
+);
 
 export default providersRouter;
