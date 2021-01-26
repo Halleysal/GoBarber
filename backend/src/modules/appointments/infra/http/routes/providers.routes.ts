64209f1383670fdf5af2904bb94074d2838eed9a
Halley SG <halleysal@gmail.com>
2021-01-26 11:12:12 -0300

    feat: data validation

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticaded from '@modules/users/infra/http/middlewares/ensureAuthenticaded';
 import ProvidersController from '../controllers/ProvidersController';
@@ -16,6 +17,11 @@ providersRouter.get('/', providersController.create);
 
 providersRouter.get(
   '/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    },
+  }),
   providerMonthAvailabilityController.create,
 );
 
