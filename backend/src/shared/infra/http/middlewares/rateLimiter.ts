3a02274f361a1718efa0962bcf633df50a289f24
Halley SG <halleysal@gmail.com>
2021-01-27 11:37:08 -0300

    feat: adding rate limit middleware

---
+++ backend/src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import AppError from 'shared/errors/AppError';
+import redis from 'redis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+
+const redisClient = redis.createClient({
+  host: process.env.RADIS_HOST,
+  port: Number(process.env.RADIS_PORT),
+  password: process.env.RADIS_PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5,
+  duration: 1,
+});
+
+export default async function rateLimiter(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(req.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests', 429);
+  }
+}
